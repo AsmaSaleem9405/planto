03a6c88e024554fff3c981b9f0742381f1fd4422
AsmaSaleem9405 <automationninga22@gmail.com>
2026-07-02 12:52:36 +0500

    add colors

--- app/components/Featuredplant.js
+++ app/components/Featuredplant.js
@@ -14,7 +14,7 @@ export default function FeaturedPlants() {
         {/* First Card */}
 
      {/* First Card */}
-<div className="relative h-[320px] rounded-[110px] border border-white/10 bg-white/5 backdrop-blur-xl overflow-visible mb-8">
+<div className="relative h-[320px] rounded-[110px] border border-white/10 bg-white/5 backdrop-blur-md overflow-visible mb-8">
 
   <div className="grid grid-cols-2 h-full">
 
@@ -23,8 +23,8 @@ export default function FeaturedPlants() {
       <Image
         src="/images/plant4.png"
         alt="Plant"
-        width={397}
-        height={395}
+        width={400}
+        height={400}
         className="absolute left-6 -top-24"
       />
     </div>
@@ -33,12 +33,11 @@ export default function FeaturedPlants() {
     <div className="flex flex-col justify-center text-white pr-10">
 
       <h3 className="text-[22px] font-medium">
-        For Small Decs Ai Plat
+        Peace Lily
       </h3>
 
       <p className="text-[11px] text-gray-300 mt-2 leading-5 max-w-[260px]">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        The Peace Lily is a beautiful indoor plant known for its glossy green leaves and elegant white flowers. It adds a fresh, modern look to homes and offices while helping improve indoor air quality. It's an excellent choice for beginners because it requires minimal care.
       </p>
 
       <h4 className="text-[30px] font-semibold mt-4">
@@ -47,13 +46,14 @@ export default function FeaturedPlants() {
 
       <div className="flex items-center gap-3 mt-4">
 
-        <button className="h-[38px] px-6 rounded-md border border-white text-sm hover:bg-green-600 transition-all duration-300">
+         <button className="h-[38px] px-6 rounded-md border border-white text-sm hover:bg-green-600 transition-all duration-300">
           Explore
         </button>
 
         <button className="h-[38px] w-[38px] flex items-center justify-center rounded-md border border-white hover:bg-green-600 transition-all duration-300">
           <FiShoppingBag size={15} />
         </button>
+
 
       </div>
 
@@ -63,20 +63,18 @@ export default function FeaturedPlants() {
 
 </div>
 {/* Second Card */}
-<div className="relative h-[320px] rounded-[110px] border border-white/10 bg-white/5 backdrop-blur-xl overflow-hidden">
+<div className="relative h-[320px] rounded-[110px] border mt-18 border-white/10 bg-white/5 backdrop-blur-md overflow-visible">
 
   <div className="grid grid-cols-2 h-full">
 
     {/* Content */}
-    <div className="flex flex-col justify-center text-white pl-10">
-
+    <div className="flex flex-col justify-center text-white pl-40">
       <h3 className="text-[22px] font-medium">
-        For Fresh Decs Ai Plat
+         Zebra Haworthia
       </h3>
 
       <p className="text-[11px] text-gray-300 mt-2 leading-5 max-w-[260px]">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Zebra Haworthia is a compact succulent with striking dark green leaves covered in white zebra-like stripes. Its unique appearance and drought tolerance make it a perfect desk, shelf, or windowsill plant for beginners and busy plant lovers.
       </p>
 
       <h4 className="text-[30px] font-semibold mt-4">
@@ -100,11 +98,11 @@ export default function FeaturedPlants() {
     {/* Plant */}
     <div className="relative flex items-center justify-center">
       <Image
-        src="/images/plant3.png"
+        src="/images/plant6.png"
         alt="Plant"
-        width={200}
-        height={200}
-        className="absolute right-8 bottom-0"
+        width={600}
+        height={460}
+        className="absolute left-6 -top-36"
       />
     </div>
 
@@ -112,8 +110,6 @@ export default function FeaturedPlants() {
 
 </div>
 </div>
-
-
     </section>
   );
 }
