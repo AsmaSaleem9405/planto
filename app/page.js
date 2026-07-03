ea6032f326715d004a7199a605954f84251d1722
AsmaSaleem9405 <automationninga22@gmail.com>
2026-07-03 18:00:42 +0500

    bg

--- app/page.js
+++ app/page.js
@@ -1,22 +1,18 @@
 import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
-import FeaturedPlants from "@/app/components/Featuredplant";
 import TopSellingPlantsPage from "@/app/components/TopSellingPlantsPage"
 
 export default function Home() {
   return (
     <main
        
-  className="relative min-h-screen bg-cover bg-center bg-no-repeat"
-  style={{
-    backgroundImage: "url('/images/bg-plant.png')",
-  }}
+ 
 >
 
       <Navbar />
 
       <Hero />
-<FeaturedPlants/>
+
 <TopSellingPlantsPage />
     </main>
     
