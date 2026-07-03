258c6a41a543ec96d9b2478e07f4deaa1ad4dfd5
AsmaSaleem9405 <automationninga22@gmail.com>
2026-07-03 17:12:04 +0500

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Navbar from "@/app/components/Navbar";
+import Hero from "@/app/components/Hero";
+import TopSellingPlants from "@/app/components/TopSellingPlantsPage";
+export default function Home() {
+  return (
+    <main
+       
+  
+>
+
+      <Navbar />
+
+      <Hero />
+      <TopSellingPlants/>
+    </main>
+    
+  );
+}
